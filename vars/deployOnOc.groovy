1c6d9078b03eaf9fd1c74580f8e8dfe8fda49457
omarshaban32 <54452908+omarshaban32@users.noreply.github.com>
2024-06-19 18:20:08 +0300

    Update deployOnOc.groovy

--- vars/deployOnOc.groovy
+++ vars/deployOnOc.groovy
@@ -5,7 +5,7 @@ def call(String openshiftCredentialsID, String nameSpace, String clusterUrl) {
     
     // Login to OpenShift using the service account token
     withCredentials([string(credentialsId: openshiftCredentialsID, variable: 'OC_TOKEN')]) {
-        sh "oc login --token=$OC_TOKEN --server=$clusterUrl "
+        sh "oc login --token=$OC_TOKEN --server=$clusterUrl --insecure-skip-tls-verify "
     }
 
     // Apply the updated deployment.yaml to the OpenShift cluster
